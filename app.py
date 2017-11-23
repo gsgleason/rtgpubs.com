ba48b3d8e1a8903a82f4a03af200ac324baa8c4e
Gregory Gleason <gsgleason@gmail.com>
2017-11-23 13:51:34 -0700

    working on adding session and db stuff for ipn

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, Markup, request
+from flask import Flask, render_template, Markup, request, session, requests
 from flask_sslify import SSLify
 import requests
+import uuid
 from config import blogger
-  
+from db import Customer, session
+
 app = Flask(__name__)
 app.config.from_object('config.flask')
 sslify = SSLify(app, permanent=True)
@@ -42,6 +44,42 @@ def blog():
 		content.append({'title':post.get('title'),'content':Markup(post.get('content'))})
 	return render_template('page.html', content=content)
 
+@app.route('/buy')
+def buy():
+	if 'id' not in session:
+		session['id'] = uuid.uuid4()
+	customer = session.query(Customer).filter(Customer.session_id == session['id']).first()
+	if customer is None:
+		customer = Customer(session_id=session['id'])
+	session.add(customer)
+	session.commit()
+	return render_template('buy.html')
+
 @app.route('/ipn', methods=['POST'])
 def ipn():
-	pass
+	email = request.form.get('payer_email')
+	paypal_transaction_id = request.form.get('txn_id')
+	payment_status = request.form.get('payment_status')
+	session_id = request.form.get('custom')
+	# first we will search by transaction ID, which would be to update an existing transaction
+	customer = session.query(Customer).filter(Customer.paypal_transaction_id == paypal_transaction_id).first()
+	# if that's not found, search by session_id, this will work for users who initiated the purchase through the site and have returned
+	if customer is None:
+		customer = session.query(Customer).filter(Customer.session_id == session_id).first()
+	# if that's not found, create new instance
+	if customer is None:
+		customer = Customer()
+		session.add(customer)
+	customer.email = email
+	customer.paypal_transaction_id = paypal_transaction_id
+	customer.payment_status = payment_status
+	customer.session_id = session_id
+	session.commit()
+
+	# send full post back to paypal
+	data = request.form
+	data['cmd'] = '_notify-validate'
+	url = 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr'
+	r = requests.post(url, data=data)
+
+	return ""
---
+++ db.py
@@ -0,0 +1,26 @@
+from config import db
+from sqlalchemy import create_engine, Column, Sring, Integer, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+Base = declarative_base()
+engine = create_engine(db.uri, convert_unicode=True)
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+class Customer(Base):
+	__tablename__ = 'customers'
+
+	id = Column(Integer, primay_key=True)
+	paypal_transaction_id = Column(String)
+	email = Column(String)
+	session_id = Column(String)
+	payment_status = Column(String)
+
+	def __repr__(self):
+		return "<Customer(id={}, email='{}', paypal_transaction_id='{}', session_id='{}', payment_status={})>".format(self.id, self.email, self.paypal_transaction_id, self.session_id, self.payment_status)
+
+def initdb():
+	Base.metadata.delete_all(engine)
+	Base.metadata.create_all(engine)
+
+
